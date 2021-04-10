0e8ac4814cab3ef3ba95c437a77d9c8600345e01
Mario Vilas <mvilas@gmail.com>
2021-04-10 12:27:14 +0200

    Added more generic platform targets.

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ LABEL description="Build environment for The Tick"
 RUN dpkg --add-architecture i386; \
     apt update
 RUN apt install -y gcc-multilib; \
-    apt install -y build-essential gcc-arm-linux-gnueabi libc6-dev-i386-amd64-cross
+    apt install -y build-essential libc6-dev-i386-amd64-cross \
+    gcc-arm-linux-gnueabi gcc-aarch64-linux-gnu \
+    gcc-mips-linux-gnu gcc-mips64-linux-gnuabi64
 RUN mkdir /opt/src /opt/bin
--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -e
 cd "$(dirname "$0")"
 mkdir -p bin
 docker build -t thetick-builder .
-for t in generic-intel-32 generic-intel-64 lexmark-cx310dn
+for t in generic-arm-32 generic-arm-64 generic-intel-32 generic-intel-64 generic-mips-32 generic-mips-64 lexmark-cx310dn
 do
     docker run -it -v $(pwd)/src:/opt/src -v $(pwd)/bin:/opt/bin thetick-builder /bin/sh -c "cd /opt/src; TARGET=$t make clean all; chown 1000:1000 /opt/bin/ticksvc-*"
 done
