773690dd3732ce8223926b0eb2ec4f93f9df34a3
Mario Vilas <mvilas@gmail.com>
2021-05-05 20:25:13 +0200

    Fixed bug that would cause two bots to be instanced with rundll32.exe instead of just one.

--- src/libmain.c
+++ src/libmain.c
@@ -24,17 +24,32 @@ DWORD WINAPI _stub_run(LPVOID lpParam);
 // The thread will be automatically locked until DLL initialization is complete.
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved __attribute__((unused)))
 {
-    switch (fdwReason) {
-        case DLL_PROCESS_ATTACH:
-            DisableThreadLibraryCalls(hinstDLL);
-            CloseHandle(CreateThread(NULL, 0, &_stub_run, NULL, 0, NULL));
-            break;
-        case DLL_PROCESS_DETACH:
-            break;
+    // Only try to run the bot on DLL load, ignore all other events.
+    if (fdwReason == DLL_PROCESS_ATTACH) {
+
+        // Disable thread creation events, since it may cause a deadlock.
+        DisableThreadLibraryCalls(hinstDLL);
+
+        // Check if we're running inside rundll32.exe.
+        char pathname[MAX_PATH];
+        memset(pathname, 0, sizeof(pathname));
+        GetModuleFileNameA(hModule, pathname, sizeof(pathname));
+        char *filename = PathFindFileNameA(pathname);
+        if (filename == NULL || filename[0] == 0 || stricmp(filename, "rundll32.exe") != 0) {
+
+            // For any process other than rundll32.exe, run the bot in a background thread.
+            // Do not wait for the thread to be initialized and just close the handle
+            // immediately, otherwise we might deadlock since we're still being loaded.
+            CloseHandle( CreateThread(NULL, 0, &_stub_run, NULL, 0, NULL) );
+
+        }
     }
+
+    // We must always return TRUE or the DLL load may be aborted.
     return TRUE;
 }
 
+// Function stub with the signature CreateThread expects.
 DWORD WINAPI _stub_run(LPVOID lpParam __attribute__((unused)))
 {
     run(0, NULL);
